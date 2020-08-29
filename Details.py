b218ff56a03c6f65a873aaf2dbb34c8d2a44df3e
fasmin <12fasminlbt19it@gmail.com>
2020-08-29 16:27:55 +0530

    Commit Q(2)

---
+++ Details.py
@@ -0,0 +1,7 @@
+def personal_details():
+    name = "Fasmin Ishak"
+    address = "Azhuvalappil House\nMalappuram\nIndia"
+    telephone_number = "8192510745"
+    print("Name: " + name + "\nAddress: " + address + "\nTelephone Number: " + telephone_number)
+
+personal_details()
