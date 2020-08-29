6f57d656f14f0e56ca4a078c92f91b476c90bfa0
fasmin <12fasminlbt19it@gmail.com>
2020-08-29 16:26:48 +0530

    fix:wrong file

--- Details.py
+++
@@ -1,7 +0,0 @@
-def personal_details():
-    name = "Fasmin Ishak"
-    address = "Azhuvalappil House\nMalappuram\nIndia"
-    telephone_number = "8192510745"
-    print("Name: " + name + "\nAddress: " + address + "\nTelephone Number: " + telephone_number)
-
-personal_details()
